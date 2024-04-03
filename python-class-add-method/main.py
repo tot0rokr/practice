3d8cdaf53a38e335de942db0d470648e8c09a1de
JHLEE <jhlee@neostack.co.kr>
2024-04-03 09:53:51 +0900

    python class add method

--- python-class-add-method/main.py
+++ python-class-add-method/main.py
@@ -4,7 +4,7 @@ import method_module
 class Cls():
     def __init__(self, x):
         self.i = x
-    def add_func(self, module):
-        for m in dir(module):
+    def add_func(self):
+        for m in dir(method_module):
             if not m.startswith('_'):
-                setattr(self, m, partial(getattr(module, m), self))
+                setattr(self, m, partial(getattr(method_module, m), self))
