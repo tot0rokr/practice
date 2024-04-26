58b1c4fcd862c6aab2446862dd17603e7d9648ba
JHLEE <jhlee@neostack.co.kr>
2024-04-26 16:23:04 +0900

    aio_pika restoring_connection

---
+++ python-aio-pika/publisher_restoring_connection.py
@@ -0,0 +1,43 @@
+import asyncio
+
+import aio_pika
+
+import random
+
+
+async def main() -> None:
+    while True:
+        try:
+            connection = await aio_pika.connect_robust(
+                    "amqp://guest:guest@localhost/",
+            )
+        except aio_pika.exceptions.AMQPConnectionError:
+            print("Retry Connection")
+            await asyncio.sleep(3)
+        else:
+            break
+
+    async with connection:
+
+        channel = await connection.channel()
+
+        for i in range(30):
+            routing_key = "test_queue" + str(random.randint(1, 3))
+            while True:
+                try:
+                    await channel.default_exchange.publish(
+                        aio_pika.Message(body=f"Hello {routing_key}".encode(), delivery_mode=aio_pika.DeliveryMode.PERSISTENT,),
+                        routing_key=routing_key,
+                    )
+                except aio_pika.exceptions.ChannelInvalidStateError:
+                    print("Retry Publish")
+                    await asyncio.sleep(3)
+                else:
+                    break
+
+            print(f"publish {routing_key}")
+            await asyncio.sleep(5)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
