aec5e1c9b8601b899067502d24999860f5a4e489
JHLEE <jhlee@neostack.co.kr>
2024-05-02 18:50:52 +0900

    asyncio add done callback

--- python-async/create_task.py
+++ python-async/create_task.py
@@ -1,14 +1,24 @@
 import asyncio
 
+
+def callback(fut):
+    try:
+        exc = fut.exception()
+        print(type(exc), exc)
+    except asyncio.CancelledError:
+        print("Cancelled")
+
 async def func(sec):
     print ("func")
     await asyncio.create_task(asyncio.sleep(sec))
+    1/0
     print ("func end")
 
 
 def init():
     print ("init")
     task = asyncio.create_task(func(5))
+    task.add_done_callback(callback)
     print ("init end")
 
     return task
